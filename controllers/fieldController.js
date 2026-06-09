6a45b6805be9244052ddab76e8b42546512053e4
Aakashgautam001 <agautam@kloudrac.com>
2026-06-09 17:13:39 +0530

    add get api for getting all fields

--- controllers/fieldController.js
+++ controllers/fieldController.js
@@ -1,177 +1,110 @@
-const { Op } = require("sequelize");
+const { Sequelize, DataTypes, QueryTypes, where, Op } = require("sequelize");
 const { responseSuccess, responseError } = require("../helper/responce");
-const {
-  NAVIGATE_TYPES,
-  getStandardFieldLabels,
-  getStandardFields,
-} = require("../constants/objectFields");
+const sendEmail = require("../common/mailer")
+const fs = require('fs');
+const path = require('path');
+const xlsx = require('xlsx');
 
 exports.storeField = async (req, res) => {
-  try {
-    const FiledBody = req.body;
+    try {
+        const FiledBody = req.body;
 
-    if (!FiledBody || FiledBody.length == 0) {
-      return await responseSuccess(req, res, "lead extra data submitted successfully");
+        if (!FiledBody || FiledBody.length == 0) {
+            return await responseSuccess(req, res, "lead extra data submitted successfully")
+        }
+
+        let fields = FiledBody.map(entry => ({
+            field_lable: entry.field_lable.trim(),
+            navigate_type: entry.navigate_type
+        }));
+
+        const existingFieldsArrayInLeads = ["Owner", "Name *", 'Name', "Status", "Source", "Lead Type", "Organization Name *", "Details", "Contact Name", "Email Id", "Personal Contact No.", "Whatsapp No.", "Official No.", "Created On", "Last Modified On", "Country", "State", "City", "Zip / Postal Code", "Address", 'Zip', 'Zip Code', 'Postal Code'];
+
+        const existingFieldsArrayInAccounts = ["Owner", "Account Name *", "Account Name", 'Name', 'Account', "Owner *", "Type *", "Parent Account", "Website", "Contact No *", "Contact", "Industry *", "Employee *", "Description", "Created On", "Last Modified On", "Billing Country *", "Billing State *", "Billing City", "Zip / Postal Code *", "Billing Address", "Make Shipping Address same as Billing Address", "Shipping Country *", "Shipping State *", "Shipping City", "Shipping Address", 'Zip Code', 'Postal Code'];
+
+        const existingFieldsArrayInContact = ["Owner *", "Owner", "Account Name *", "Account Name", "Name", "Saluation", "First Name *", "First Name", "Middle Name", "Last Name *", "Last Name", "Designation", "Contact No*", "Contact No", "Email", "Email Id", "Fax", "Created On", "Last Modified On", "Mailing Country", "Mailing State", "Mailing City", "Zip / Postal Code", "Mailing Full Address"];
+
+        const getCustomArray = (navigateType) => {
+            switch (navigateType) {
+                case 'lead':
+                    return existingFieldsArrayInLeads;
+                case 'accounts':
+                    return existingFieldsArrayInAccounts;
+                case 'contact':
+                    return existingFieldsArrayInContact;
+                default:
+                    return [];
+            }
+        };
+
+        const customDuplicateEntries = fields.filter(field => {
+            const customArray = getCustomArray(field.navigate_type);
+            return customArray.includes(field.field_lable);
+        });
+
+        if (customDuplicateEntries.length > 0) {
+            return await responseError(req, res, `${customDuplicateEntries.length} fields are duplicates default fields.`);
+        }
+
+        // Check for duplicates in the database
+        let duplicateEntries = await req.config.Field.findAll({
+            where: {
+                [Op.and]: fields.map(field => ({
+                    field_lable: field.field_lable.trim(),
+                    navigate_type: field.navigate_type
+                }))
+            },
+            attributes: ['field_lable', 'navigate_type']
+        });
+
+        if (duplicateEntries.length > 0) {
+            return await responseError(req, res, `${duplicateEntries.length} fields are duplicate.`)
+        }
+
+        let FieldData = await req.config.Field.bulkCreate(FiledBody, {
+            updateOnDuplicate: ["field_id", "field_lable", "navigate_type", "field_name", "field_order", "option", "input_value", "input_type", "field_type", "field_size"]
+        });
+
+        await responseSuccess(req, res, "lead extra data submitted successfully", FieldData);
+    } catch (error) {
+        logErrorToFile(error);
+        console.log(error);
+        await responseError(req, res, "Something Went Wrong");
     }
-
-    const fields = FiledBody.map((entry) => ({
-      field_lable: entry.field_lable.trim(),
-      navigate_type: entry.navigate_type,
-    }));
-
-    const customDuplicateEntries = fields.filter((field) => {
-      const standardLabels = getStandardFieldLabels(field.navigate_type);
-      return standardLabels.includes(field.field_lable);
-    });
-
-    if (customDuplicateEntries.length > 0) {
-      return await responseError(
-        req,
-        res,
-        `${customDuplicateEntries.length} fields are duplicates default fields.`
-      );
-    }
-
-    const duplicateEntries = await req.config.Field.findAll({
-      where: {
-        [Op.and]: fields.map((field) => ({
-          field_lable: field.field_lable.trim(),
-          navigate_type: field.navigate_type,
-        })),
-      },
-      attributes: ["field_lable", "navigate_type"],
-    });
-
-    if (duplicateEntries.length > 0) {
-      return await responseError(req, res, `${duplicateEntries.length} fields are duplicate.`);
-    }
-
-    const FieldData = await req.config.Field.bulkCreate(FiledBody, {
-      updateOnDuplicate: [
-        "field_id",
-        "field_lable",
-        "navigate_type",
-        "field_name",
-        "field_order",
-        "option",
-        "input_value",
-        "input_type",
-        "field_type",
-        "field_size",
-      ],
-    });
-
-    await responseSuccess(req, res, "lead extra data submitted successfully", FieldData);
-  } catch (error) {
-    logErrorToFile(error);
-    console.log(error);
-    await responseError(req, res, "Something Went Wrong");
-  }
 };
 
 exports.getField = async (req, res) => {
-  try {
-    let FieldData;
-    const id = req.query.id;
-    if (id) {
-      FieldData = await req.config.Field.findByPk(id);
-    } else {
-      FieldData = await req.config.Field.findAll({
-        where: {
-          navigate_type: req.query.nav_type,
-        },
-        order: [["field_order", "ASC"]],
-      });
+    try {
+        let FieldData
+        const id = req.query.id
+        if (id) {
+            FieldData = await req.config.Field.findByPk(id)
+        } else {
+            FieldData = await req.config.Field.findAll({
+                where: {
+                    navigate_type: req.query.nav_type
+                },
+                order: [['field_order', 'ASC']]
+            })
+        }
+
+        await responseSuccess(req, res, "Field Data", FieldData)
+    } catch (error) {
+        logErrorToFile(error)
+        console.log(error)
+        await responseError(req, res, "Something Went Wrong")
     }
-
-    await responseSuccess(req, res, "Field Data", FieldData);
-  } catch (error) {
-    logErrorToFile(error);
-    console.log(error);
-    await responseError(req, res, "Something Went Wrong");
-  }
-};
-
-exports.getAllObjectFields = async (req, res) => {
-  try {
-    const navType = req.query.nav_type || req.query.navigate_type;
-
-    if (!navType) {
-      return await responseError(
-        req,
-        res,
-        `nav_type is required. Valid values: ${NAVIGATE_TYPES.join(", ")}`
-      );
-    }
-
-    if (!NAVIGATE_TYPES.includes(navType)) {
-      return await responseError(
-        req,
-        res,
-        `Invalid nav_type '${navType}'. Valid values: ${NAVIGATE_TYPES.join(", ")}`
-      );
-    }
-
-    const standardFields = getStandardFields(navType);
-
-    const customFieldRows = await req.config.Field.findAll({
-      where: { navigate_type: navType },
-      order: [["field_order", "ASC"]],
-    });
-
-    const customFields = customFieldRows.map((row) => {
-      const plain = row.toJSON ? row.toJSON() : row;
-      return {
-        ...plain,
-        is_standard: false,
-        field_type: plain.field_type || "custom",
-      };
-    });
-
-    const allFields = [
-      ...standardFields,
-      ...customFields.map((field) => ({
-        field_id: field.field_id,
-        navigate_type: field.navigate_type,
-        field_lable: field.field_lable,
-        field_name: field.field_name,
-        field_order: field.field_order,
-        option: field.option,
-        input_value: field.input_value,
-        input_type: field.input_type,
-        field_type: field.field_type,
-        field_size: field.field_size,
-        is_standard: false,
-        createdAt: field.createdAt,
-        updatedAt: field.updatedAt,
-      })),
-    ];
-
-    await responseSuccess(req, res, "All object fields fetched successfully", {
-      navigate_type: navType,
-      standard_count: standardFields.length,
-      custom_count: customFields.length,
-      total_count: allFields.length,
-      standard_fields: standardFields,
-      custom_fields: customFields,
-      all_fields: allFields,
-    });
-  } catch (error) {
-    logErrorToFile(error);
-    console.log(error);
-    await responseError(req, res, "Something Went Wrong");
-  }
-};
+}
 
 exports.deleteField = async (req, res) => {
-  try {
-    const FieldData = await req.config.Field.findByPk(req.query.id);
-    if (!FieldData) responseError(req, res, "field not found");
-    await FieldData.destroy();
-    await responseSuccess(req, res, "Field Data deleted");
-  } catch (error) {
-    logErrorToFile(error);
-    console.log(error);
-    await responseError(req, res, "Something Went Wrong");
-  }
-};
+    try {
+        const FieldData = await req.config.Field.findByPk(req.query.id)
+        if (!FieldData) responseError(req, res, "field not found")
+        await FieldData.destroy()
+        await responseSuccess(req, res, "Field Data deleted")
+    } catch (error) {
+        logErrorToFile(error)
+        console.log(error)
+        await responseError(req, res, "Something Went Wrong")
+    }
+}
--- routes/FieldRoutes.js
+++ routes/FieldRoutes.js
@@ -8,10 +8,6 @@ const router = express.Router();
 
 //admin routes
 router
-    .route("/all")
-        .get(resolver, fieldController.getAllObjectFields)
-
-router
     .route("/")
         .post(resolver,  fieldController.storeField)
         .get(resolver,  fieldController.getField)
